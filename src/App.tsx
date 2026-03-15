b9a149de7cb7944219a905df7813d3bc81a68df0
PranoyAcharyya <acharyapranoy@gmail.com>
2026-03-16 00:14:59 +0530

    push

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,65 @@
-
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 import { fetchProducts } from "./redux-toolkit/slices/productlistSlice";
-import { useAppDispatch, useAppSelector } from './redux-toolkit/hooks/hook';
-import Grid from '@mui/material/Grid';
+import { useAppDispatch, useAppSelector } from "./redux-toolkit/hooks/hook";
+import Grid from "@mui/material/Grid";
 import ProductCard from "./components/ProductCard";
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { Badge } from '@mui/material';
-import DialogCart from './components/DialogCart';
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { Badge } from "@mui/material";
+import DialogCart from "./components/DialogCart";
 
 function App() {
-  const [open,setOpen] = useState(false);
-  const {products,status} = useAppSelector((state) => state.products);
+  const [open, setOpen] = useState(false);
+  const { products, status } = useAppSelector((state) => state.products);
   const dispatch = useAppDispatch();
-  const {cartItems} = useAppSelector(s => s.cart);
+  const { cartItems } = useAppSelector((s) => s.cart);
 
-
-  
-    const handleClickOpen = () => {
+  const handleClickOpen = () => {
     setOpen(true);
   };
 
   const handleClose = () => {
     setOpen(false);
   };
-  
+
   useEffect(() => {
     dispatch(fetchProducts());
-  }, [])
+  }, []);
 
-  if(status === "pending"){
-    return <h2>Loading...</h2>
+  if (status === "pending") {
+    return <h2>Loading...</h2>;
   }
-
-  
 
   return (
     <>
       <Grid container spacing={3} padding={3}>
+        {products.map((product) => (
+          <Grid size={{ xs: 12, sm: 6, lg: 4 }} key={product.id}>
+            <ProductCard product={product} />
+          </Grid>
+        ))}
+      </Grid>
 
-      {products.map((product) => (
-        <Grid  size={{ xs: 12, sm: 6 ,lg:4}} key={product.id}>
-          <ProductCard product={product} />
-        </Grid>
-      ))}
-
-    </Grid>
-
-    <Badge onClick={handleClickOpen} color="secondary" badgeContent={cartItems.length} max={999} sx={{position:'fixed',bottom:'20px',right:'120px'}}>
- <ShoppingCartIcon />
-</Badge>
-<DialogCart open={open} handleClose={handleClose}/>
+      <Badge
+        onClick={handleClickOpen}
+        color="secondary"
+        badgeContent={cartItems.length}
+        max={10}
+        sx={{
+          position: "fixed",
+          bottom: "20px",
+          right: "120px",
+          padding: "20px",
+          backgroundColor: "#fff",
+          borderRadius:"100%",
+          boxShadow:"0px 0px 20px 20px #00000030"
+        }}
+      >
+        <ShoppingCartIcon />
+      </Badge>
+      <DialogCart open={open} handleClose={handleClose} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
